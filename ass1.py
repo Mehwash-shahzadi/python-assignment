1020d61bea8948b86706eb5bc5b06c508370b185
mehwash shahzadi <mehwashshahzadi@gmail.com>
2025-01-19 18:59:44 +0500

    file

---
+++ ass1.py
@@ -0,0 +1,35 @@
+#print the version the python which you are using.
+import sys
+print(sys.version)
+# five variables in same line
+a,b,c,d,e=10,20,30,40,50
+#declare a variable ,check its type, and display its ID
+name="Mehwash shahzadi"
+print(type(name))
+print(id(name))
+#write a program in python that uses variables to perform a sum equal t0 44.
+a=12
+b=12
+print(a+b)
+#calculate the radius of circle.
+import math 
+radius=5
+area= math.pi *(radius**2)
+print(area)
+#print the user first name and last name.
+first_name="Mehwash"
+last_name="Shahzadi"
+print(first_name + " "+ last_name)
+
+#is the following assignment statement valid or invalid.if it is invalid ,why?72=amount
+"""its not valid because variable name not start with number it always start with letter and undescore."""
+#is the variable name is Sales the same as the sales?why or why not.
+"""they are not same.Sales and sales are two different variables.The variables names are the case sensitive"""
+#what will be displayed by the followingmy_value=99 ,my_value=0 
+"""firstly my_value is 99.and then reassign the value 0.so the output will be 0"""
+# write the python statement that assign the sum of 10 and 14 to the variable total.
+"""total=10+14"""
+#write a python statement that subtract the variable down_payment from variable total and assign the result to the variable due.
+"""due=total-down_payment"""
+#write a python statement that multiplies the variable subtotal by 0.15 and assign the result to the variable total.
+""""total=subtotal*0.15"""
